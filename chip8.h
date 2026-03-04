bed79d001c51987ee31bfab6e0cf16e173b4a84c
Karol Nowocień <newshadowcharles@gmail.com>
2026-03-04 21:23:29 +0100

    Implement Chip8 constructor to initialize program counter and load fontset into memory

--- chip8.h
+++ chip8.h
@@ -6,7 +6,18 @@
 class Chip8
 {
 public:
+  Chip8();
+
 private:
+  uint8_t memory[4096]{};
+  uint8_t registers[16]{};
+  uint16_t index{};
+  uint16_t pc{};
+  uint16_t stack[16]{};
+  uint8_t sp{};
+  uint8_t delayTimer{};
+  uint8_t soundTimer{};
+  uint16_t opcode{};
 };
 
 #endif
